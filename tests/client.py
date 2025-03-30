f2aa13eb5c32d2c8c2dfd0bc282528ebbcf75c26
pechenka <kmd240304@gmail.com>
2025-03-30 14:38:50 +0700

    add html, update main and client files

--- tests/client.py
+++ tests/client.py
@@ -1,6 +1,8 @@
 import requests
 
 r = requests.get('http://localhost:5000/')
-
+data_to_r = requests.get('http://localhost:5000/data_to')
 print(r.status_code)
-print(r.text) 
+print(r.text)
+print(data_to_r.status_code)
+print(data_to_r.text) 
