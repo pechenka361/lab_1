8b12deb80e84afcb8b8e54166d6cea3f77f829ef
pechenka <kmd240304@gmial.com>
2025-04-05 19:42:29 +0700

    update build script

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -11,6 +11,6 @@ with open(path, "rb") as fh:
     b64 = base64.b64encode(img_data)
 
 jsondata = {"imagebin": b64.decode("utf-8")}
-res = requests.post("http://loclahost:5000/apinet", json=jsondata)
+res = requests.post("http://localhost:5000/apinet", json=jsondata)
 if res.ok:
     print(res.json())
