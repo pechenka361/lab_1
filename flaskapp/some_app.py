eac97adf69f98b014e79708e36676c1a37108b7d
pechenka <kmd240304@gmial.com>
2025-04-13 11:46:38 +0700

    del jpeg

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -47,7 +47,7 @@ class NetForm(FlaskForm):
         "Load image",
         validators=[
             FileRequired(),
-            FileAllowed(["jpg", "png", "jpeg"], "Image only!0"),
+            FileAllowed(["jpg", "png"], "Image only!0"),
         ],
     )
     recaptcha = RecaptchaField()
