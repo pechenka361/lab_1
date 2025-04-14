9231e584a48a23d80e7f82441736ab1c11a6eb66
pechenka <kmd240304@gmial.com>
2025-04-14 20:11:13 +0700

    resize image

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, Response, flash
 from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, TextAreaField, IntegerField
+from wtforms.validators import DataRequired, Optional, NumberRange
 from flask_wtf.file import FileField, FileAllowed, FileRequired
 from flask_bootstrap import Bootstrap
 from werkzeug.utils import secure_filename
@@ -107,23 +107,57 @@ class ResizeForm(FlaskForm):
             FileAllowed(["jpg", "png", "jpeg", "gif"], message="Разрешены только изображения (JPG, PNG, JPEG, GIF)!"),
         ],
     )
+    percent = IntegerField(
+        "Проценты (%)",
+        validators=[Optional(), NumberRange(min=1, max=500, message="Введите значение от 1 до 500")],
+    )
+    width = IntegerField(
+        "Ширина (px)",
+        validators=[Optional(), NumberRange(min=1, max=5000, message="Введите знаячение от 1 до 5000")],
+    )
+    height = IntegerField(
+        "Высота (px)",
+        validators=[Optional(), NumberRange(min=1, max=5000, message="Введите значение от 1 до 5000")],
+    )
     submit = SubmitField("Загрузить")
+
+def resize_image(image_path, output_path, mode, percent=None, width=None, height=None, keep_aspect_ratio=True):
+    with Image.open(image_path) as img: 
+        if mode == "percent":
+            if percent is None:
+                raise ValueError("необходимо указать процент для изменения размера")
+            new_width = int(img.width*percent / 100)
+            new_height = int(img.height*percent / 100)
+        elif mode == "pixels":
+            if width is None or height is None:
+                raise ValueError("Необходимо указать ширину и высоту для изменения размера")
+            new_width = width
+            new_height = height
+            if keep_aspect_ratio:
+                aspect_ratio = img.width/img.height
+                if width / height > aspect_ratio:
+                    new_width = int(height*aspect_ratio)
+                else:
+                    new_height = int(width/aspect_ratio)
+        else: 
+            raise ValueError("Неверный режим изменения размера")
+        
+        resized_img = img.resize((new_width, new_height), Image.ANTIALIAS)
+        resize_image.save(output_path)
 
 @app.route("/image_resize", methods=['GET', 'POST'])
 def image_resize():
     form = ResizeForm()
     filename = None
+    resize_filename = None
 
     if form.validate_on_submit():
-        # Проверка наличия директории ./static
         upload_folder = "./static"
         if not os.path.exists(upload_folder):
-            os.makedirs(upload_folder)  # Создаем директорию, если её нет
+            os.makedirs(upload_folder)
 
-        # Получение файла из формы
         uploaded_file = form.upload.data
         if uploaded_file:
-            # Проверка расширения файла
             allowed_extensions = {'png', 'jpg', 'jpeg', 'gif'}
             file_extension = uploaded_file.filename.rsplit('.', 1)[-1].lower()
             if file_extension not in allowed_extensions:
@@ -133,11 +167,30 @@ def image_resize():
             # Сохранение файла
             try:
                 filename = secure_filename(uploaded_file.filename)
-                file_path = os.path.join(upload_folder, filename)
-                uploaded_file.save(file_path)
-                flash("Файл успешно загружен.", "success")
+                original_path = os.path.join(upload_folder, filename)
+                uploaded_file.save(original_path)
+                mode = request.form.get("resize_mode")
+                percent = form.percent.data
+                width = form.width.data
+                height = form.height.data
+                keep_aspect_ratio = request.form.get("keep_aspect_ratio") == "on"
+
+                name, ext = os.path.splitext(filename)
+                resized_filename = f"{name}_resized{ext}"
+                resize_path = os.path.join(upload_folder, resize_filename)
+
+                resize_image(
+                    image_path=original_path,
+                    output_path=resize_path,
+                    mode=mode,
+                    percent=percent,
+                    width=width,
+                    height=height,
+                    keep_aspect_ratio=keep_aspect_ratio
+                )
+                flash("Изображение успешно изменено.", "success")
             except Exception as e:
-                flash(f"Ошибка при сохранении файла: {e}", "danger")
+                flash(f"Ошибка при изменении изображения: {e}", "danger")
         else:
             flash("Файл не был загружен.", "warning")
 
