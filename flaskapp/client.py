10a76dfc52c1fad501a698615ae88e9c11d2f03f
pechenka <kmd240304@gmail.com>
2025-04-22 19:07:56 +0700

    del hello world and add comments

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,7 +1,7 @@
 import requests
 
 try:
-    r = requests.get('http://127.0.0.1:5000/')
+    r = requests.get('http://127.0.0.1:5000/hello_world')
     print(r.status_code)
     if r.status_code != 200:
         exit(1)
